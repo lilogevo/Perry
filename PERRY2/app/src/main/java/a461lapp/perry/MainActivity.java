dc6e780eb1d806c298b77cac8573f63f183136a5
royceli804 <royceli804@hotmail.com>
2016-11-12 20:56:13 -0600

    Example Button
    Uses an intent to change between main activity and date activity

--- PERRY2/app/src/main/java/a461lapp/perry/MainActivity.java
+++ PERRY2/app/src/main/java/a461lapp/perry/MainActivity.java
@@ -1,14 +1,26 @@
 package a461lapp.perry;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Button changeActivityExampleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        changeActivityExampleButton = (Button) findViewById(R.id.ex);
+        changeActivityExampleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Date.class);
+                startActivity(i);
+            }
+        });
     }
 }
